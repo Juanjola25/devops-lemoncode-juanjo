cf22ec2738c3e0ec4663392169e64167aab4b5ae
Juanjola25 <juanjolarafuentes@gmail.com>
2026-05-19 13:38:06 +0200

    File finished

--- 00_Fundamentos_Linux/script.sh
+++ 00_Fundamentos_Linux/script.sh
@@ -10,6 +10,6 @@ mkdir -p foo/dummy foo/empty
 echo "$texto" > foo/dummy/file1.txt
 touch foo/dummy/file2.txt
 
-cp foo/dummy/file1.txt foo/dummy/file2.txt
+cat foo/dummy/file1.txt > foo/dummy/file2.txt
 
 mv foo/dummy/file2.txt foo/empty/
