63ddea42c7eb6e556282378dd4c253e139c711b2
Juanjola25 <juanjolarafuentes@gmail.com>
2026-05-19 13:33:56 +0200

    File editted

--- 00_Fundamentos_Linux/script.sh
+++ 00_Fundamentos_Linux/script.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-if [ -z "$1"]; then 
+if [ -z "$1" ]; then
     texto="Que me gusta la bash!!"
-    else
-        texto="$1"
-    fi
-    
-    
-mkdir -p foo\dummy foo\empty
-edit "$texto" > foo\dummy\file1.txt
-edit nul foo\dummy\file2.txt
+else
+    texto="$1"
+fi
 
-copy foo\dummyfile1.txt > foo\dummy\file2.txt
+mkdir -p foo/dummy foo/empty
+echo "$texto" > foo/dummy/file1.txt
+touch foo/dummy/file2.txt
 
+cp foo/dummy/file1.txt foo/dummy/file2.txt
 
-move foo\dummy\file2.txt foo\empty
+mv foo/dummy/file2.txt foo/empty/
